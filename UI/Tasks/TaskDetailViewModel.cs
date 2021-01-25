f747a26048440952b37a94808bb37fb5d88067d7
DanielF <fdaniel02@gmail.com>
2021-01-25 21:49:37 +0100

    basic layout added to the taskdetail

--- UI/Tasks/TaskDetailViewModel.cs
+++ UI/Tasks/TaskDetailViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace UI.Tasks
+﻿using System.Linq;
+using TaskMaster.Domain.Models;
+using TaskMaster.Services;
+
+namespace UI.Tasks
 {
     public class TaskDetailViewModel : BindableBase
     {
+        private readonly IProjectService _projectService;
+        private Project _project;
+
+        public TaskDetailViewModel(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        public Project Project
+        {
+            get => _project;
+            set => SetProperty(ref _project, value);
+        }
+
+        public async void Load()
+        {
+            var projects = await _projectService.GetProjects();
+            Project = projects.LastOrDefault();
+        }
     }
 }
