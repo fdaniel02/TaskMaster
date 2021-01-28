645b3aca14ed58c13d95936849094ed273a32140
DanielF <fdaniel02@gmail.com>
2021-01-28 21:36:37 +0100

    project states mapped to enum

---
+++ Services/Enums/ProjectStates.cs
@@ -0,0 +1,13 @@
+﻿namespace Services.Enums
+{
+    public enum ProjectStates
+    {
+        Inbox = 1,
+        Next = 2,
+        Scheduled = 3,
+        Waiting = 4,
+        Delegated = 5,
+        Later = 6,
+        Closed = 9,
+    }
+}
