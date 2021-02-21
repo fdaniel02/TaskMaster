2cfb0878a8f6aa99bdc18900f535ffa4b58ca0cb
DanielF <fdaniel02@gmail.com>
2021-02-21 19:24:24 +0100

    status and priprities added to the project db migration added

---
+++ Domain/Migrations/20210221181958_AddPriorityAndSource.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskMaster.Domain.Migrations
+{
+    public partial class AddPriorityAndSource : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Source",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "Project");
+        }
+    }
+}
