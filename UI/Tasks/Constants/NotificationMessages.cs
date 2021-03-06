2acfbfe98b81b7c03593b31010b6155bfbae4358
DanielF <fdaniel02@gmail.com>
2021-03-06 21:21:39 +0100

    new notification messages added

--- UI/Tasks/Constants/NotificationMessages.cs
+++ UI/Tasks/Constants/NotificationMessages.cs
@@ -1,7 +1,17 @@
 ﻿namespace UI.Tasks.Constants
 {
-    public class NotificationMessages
+    public static class NotificationMessages
     {
         public const string SaveSuccess = "Successfully saved!";
+
+        public const string UnknownError = "Unknown error.";
+
+        public const string SaveProjectFirst = "Please save the Project first.";
+
+        public const string ActionItemCannotBeAdded = "Action item cannot be added!";
+
+        public const string ActionItemSaveErrorMissingProject = $"{ActionItemCannotBeAdded} {SaveProjectFirst}";
+
+        public const string ActionItemSaveErrorUnknown = $"{ActionItemCannotBeAdded} {UnknownError}";
     }
 }
