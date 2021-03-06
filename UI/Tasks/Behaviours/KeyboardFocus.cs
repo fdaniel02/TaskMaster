8d43de01bff764f05acb48fb4be4162537ef35a7
DanielF <fdaniel02@gmail.com>
2021-03-06 22:55:07 +0100

    new keyboardfocus dependency property added

---
+++ UI/Tasks/Behaviours/KeyboardFocus.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace UI.Tasks.Behaviours
+{
+    public static class KeyboardFocus
+    {
+        public static readonly DependencyProperty OnProperty;
+
+        static KeyboardFocus()
+        {
+            OnProperty =
+                DependencyProperty.RegisterAttached(
+                    "On",
+                    typeof(FrameworkElement),
+                    typeof(KeyboardFocus),
+                    new PropertyMetadata(OnSetCallback));
+        }
+
+        public static void SetOn(UIElement element, FrameworkElement value)
+        {
+            element.SetValue(OnProperty, value);
+        }
+
+        public static FrameworkElement GetOn(UIElement element)
+        {
+            return (FrameworkElement)element.GetValue(OnProperty);
+        }
+
+        private static void OnSetCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            var frameworkElement = (FrameworkElement)dependencyObject;
+            var target = GetOn(frameworkElement);
+
+            if (target == null)
+            {
+                return;
+            }
+
+            frameworkElement.TargetUpdated += (s, e) => Keyboard.Focus(target);
+        }
+    }
+}
