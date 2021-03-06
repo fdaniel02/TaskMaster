8ecce3f9f889cbf07d99fae6242626d5093c7613
DanielF <fdaniel02@gmail.com>
2021-03-06 21:09:17 +0100

    notificationColorConverter added change notification color according to notification type

---
+++ UI.Test/Notifications/Converters/NotificationColorConverterTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Media;
+using FluentAssertions;
+using UI.Notifications.Converters;
+using UI.Notifications.Enums;
+using Xunit;
+
+namespace UI.Test.Notifications.Converters
+{
+    public class NotificationColorConverterTest
+    {
+        [Theory]
+        [InlineData(NotificationTypes.Error, 207, 0, 15)]
+        [InlineData(NotificationTypes.Warning, 240, 84, 30)]
+        [InlineData(NotificationTypes.Success, 0, 153, 68)]
+        [InlineData(NotificationTypes.Info, 99, 192, 223)]
+        [InlineData((NotificationTypes)int.MaxValue, 99, 192, 223)]
+        public void Convert_ReturnsColor(NotificationTypes type, byte r, byte g, byte b)
+        {
+            var sut = new NotificationColorConverter();
+
+            var actual = sut.Convert(type, null, null, null) as SolidColorBrush;
+
+            actual.Color.R.Should().Be(r);
+            actual.Color.G.Should().Be(g);
+            actual.Color.B.Should().Be(b);
+        }
+
+        [Fact]
+        public void Convert_InputNull_ReturnsInfoColor()
+        {
+            var sut = new NotificationColorConverter();
+
+            var actual = sut.Convert(null, null, null, null) as SolidColorBrush;
+
+            actual.Color.R.Should().Be(99);
+            actual.Color.G.Should().Be(192);
+            actual.Color.B.Should().Be(223);
+        }
+
+        [Fact]
+        public void Convert_InputNotNotificationType_ReturnsInfoColor()
+        {
+            var sut = new NotificationColorConverter();
+
+            var actual = sut.Convert("Something", null, null, null) as SolidColorBrush;
+
+            actual.Color.R.Should().Be(99);
+            actual.Color.G.Should().Be(192);
+            actual.Color.B.Should().Be(223);
+        }
+    }
+}
---
+++ UI/Notifications/Converters/NotificationColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using UI.Notifications.Enums;
+
+namespace UI.Notifications.Converters
+{
+    public class NotificationColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var brush = new SolidColorBrush();
+            if (value is not NotificationTypes notificationType)
+            {
+                brush.Color = Color.FromRgb(99, 192, 223);
+                return brush;
+            }
+
+            brush.Color = ConvertTypeToColor(notificationType);
+
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Color ConvertTypeToColor(NotificationTypes priority) =>
+           priority switch
+           {
+               NotificationTypes.Error => Color.FromRgb(207, 0, 15),
+               NotificationTypes.Warning => Color.FromRgb(240, 84, 30),
+               NotificationTypes.Success => Color.FromRgb(0, 153, 68),
+               _ => Color.FromRgb(99, 192, 223),
+           };
+    }
+}
