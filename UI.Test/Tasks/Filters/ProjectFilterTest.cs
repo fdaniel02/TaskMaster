af046c781509e64fb708499fcfd4ab2bbf141ae3
DanielF <fdaniel02@gmail.com>
2021-03-07 12:25:49 +0100

    project filter added

---
+++ UI.Test/Tasks/Filters/ProjectFilterTest.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Enums;
+using Domain.Models;
+using FluentAssertions;
+using UI.Tasks.Filters;
+using Xunit;
+
+namespace UI.Test.Tasks.Filters
+{
+    public class ProjectFilterTest
+    {
+        [Theory]
+        [InlineData("Test", ProjectStates.Next, "", false, true)]
+        [InlineData("Test", ProjectStates.Closed, "", false, false)]
+        [InlineData("Test", ProjectStates.Closed, "", true, true)]
+        [InlineData("Test", ProjectStates.Closed, "A", true, false)]
+        [InlineData("Test", ProjectStates.Active, "", false, true)]
+        [InlineData("Test", ProjectStates.Active, "A", false, false)]
+        [InlineData("Test", ProjectStates.Active, "Test", false, true)]
+        [InlineData("Test", ProjectStates.Active, "Testa", false, false)]
+        [InlineData("Test", ProjectStates.Active, "T est", false, false)]
+        [InlineData("", ProjectStates.Active, "", false, true)]
+        [InlineData("", ProjectStates.Active, "Test", false, false)]
+        public void FilterProject_ReturnsFilteredProject(
+            string name,
+            ProjectStates state,
+            string searchTerm,
+            bool showClosed,
+            bool expected)
+        {
+            Project project = new()
+            {
+                Name = name,
+                State = state,
+            };
+
+            var sut = new ProjectFilter();
+
+            var actual = sut.FilterProject(project, searchTerm, showClosed);
+
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ UI/Tasks/Filters/ProjectFilter.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Enums;
+using Domain.Models;
+
+namespace UI.Tasks.Filters
+{
+    public class ProjectFilter : IProjectFilter
+    {
+        public bool FilterProject(Project project, string searchExpression, bool showClosedProjects)
+            => FilterSearchExpression(project, searchExpression) && FilterClosedProjects(project, showClosedProjects);
+
+        private static bool FilterSearchExpression(Project project, string searchExpression)
+        {
+            if (string.IsNullOrWhiteSpace(searchExpression))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrWhiteSpace(project.Name))
+            {
+                return false;
+            }
+
+            return project.Name.Contains(searchExpression);
+        }
+
+        private static bool FilterClosedProjects(Project project, bool showClosedProjects)
+        {
+            return showClosedProjects || project.State != ProjectStates.Closed;
+        }
+    }
+}
