8c0b36d611b4bff659651eee27b02641613c3a41
DanielF <fdaniel02@gmail.com>
2021-03-27 21:35:05 +0100

    TagService with mocked functionality added

---
+++ Services/TagService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Services
+{
+    public class TagService : ITagService
+    {
+        public List<string> GetTags()
+        {
+            return new() { "Test1", "Test2", "TEst3", "Test3" };
+        }
+    }
+}
