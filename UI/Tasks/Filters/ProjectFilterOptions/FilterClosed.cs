7a112898b98cd3e96daa027ea26237a06b3d498a
DanielF <fdaniel02@gmail.com>
2021-03-28 19:03:48 +0200

    project filters decoupled

---
+++ UI/Tasks/Filters/ProjectFilterOptions/FilterClosed.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.GuardClauses;
+using Domain.Enums;
+using Domain.Models;
+
+namespace UI.Tasks.Filters.ProjectFilterOptions
+{
+    public class FilterClosed : IProjectFilterOption
+    {
+        public bool Filter(Project project, ProjectFilterArgs args)
+        {
+            Guard.Against.Null(project, nameof(project));
+
+            return args is null || args.ShowClosed || project.State != ProjectStates.Closed;
+        }
+    }
+}
---
+++ UI/Tasks/Filters/ProjectFilterOptions/IProjectFilterOption.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace UI.Tasks.Filters.ProjectFilterOptions
+{
+    public interface IProjectFilterOption
+    {
+        bool Filter(Project project, ProjectFilterArgs args);
+    }
+}
