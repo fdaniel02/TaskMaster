d727def5fb4d1e2018a9e0fdd476821dc4d53914
DanielF <fdaniel02@gmail.com>
2021-03-28 10:43:10 +0200

    test update

--- UI.Test/Tasks/Filters/ProjectFilterTest.cs
+++ UI.Test/Tasks/Filters/ProjectFilterTest.cs
@@ -8,22 +8,25 @@ namespace UI.Test.Tasks.Filters
 {
     public class ProjectFilterTest
     {
+        // TODO: tests needed for tagFilter. However these tests are too complex,
+        // we need to separate the filters to write the tests
         [Theory]
-        [InlineData("Test", ProjectStates.Next, "", false, true)]
-        [InlineData("Test", ProjectStates.Closed, "", false, false)]
-        [InlineData("Test", ProjectStates.Closed, "", true, true)]
-        [InlineData("Test", ProjectStates.Closed, "A", true, false)]
-        [InlineData("Test", ProjectStates.Active, "", false, true)]
-        [InlineData("Test", ProjectStates.Active, "A", false, false)]
-        [InlineData("Test", ProjectStates.Active, "Test", false, true)]
-        [InlineData("Test", ProjectStates.Active, "Testa", false, false)]
-        [InlineData("Test", ProjectStates.Active, "T est", false, false)]
-        [InlineData("", ProjectStates.Active, "", false, true)]
-        [InlineData("", ProjectStates.Active, "Test", false, false)]
+        [InlineData("Test", ProjectStates.Next, "", "", false, true)]
+        [InlineData("Test", ProjectStates.Closed, "", "", false, false)]
+        [InlineData("Test", ProjectStates.Closed, "", "", true, true)]
+        [InlineData("Test", ProjectStates.Closed, "A", "", true, false)]
+        [InlineData("Test", ProjectStates.Active, "", "", false, true)]
+        [InlineData("Test", ProjectStates.Active, "A", "", false, false)]
+        [InlineData("Test", ProjectStates.Active, "Test", "", false, true)]
+        [InlineData("Test", ProjectStates.Active, "Testa", "", false, false)]
+        [InlineData("Test", ProjectStates.Active, "T est", "", false, false)]
+        [InlineData("", ProjectStates.Active, "", "", false, true)]
+        [InlineData("", ProjectStates.Active, "Test", "", false, false)]
         public void FilterProject_ReturnsFilteredProject(
             string name,
             ProjectStates state,
             string searchTerm,
+            string tagFilter,
             bool showClosed,
             bool expected)
         {
@@ -35,7 +38,7 @@ namespace UI.Test.Tasks.Filters
 
             var sut = new ProjectFilter();
 
-            var actual = sut.FilterProject(project, searchTerm, showClosed);
+            var actual = sut.FilterProject(project, searchTerm, tagFilter, showClosed);
 
             actual.Should().Be(expected);
         }
