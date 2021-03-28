5cdc0ecaf0410deb26fda07953380f36897f1cd2
DanielF <fdaniel02@gmail.com>
2021-03-28 19:03:55 +0200

    unit test update

--- UI.Test/Tasks/Filters/ProjectFilterTest.cs
+++ UI.Test/Tasks/Filters/ProjectFilterTest.cs
@@ -1,46 +1,43 @@
-﻿using Domain.Enums;
+﻿using System.Collections.Generic;
 using Domain.Models;
 using FluentAssertions;
+using Moq;
 using UI.Tasks.Filters;
+using UI.Tasks.Filters.ProjectFilterOptions;
 using Xunit;
 
 namespace UI.Test.Tasks.Filters
 {
     public class ProjectFilterTest
     {
-        // TODO: tests needed for tagFilter. However these tests are too complex,
-        // we need to separate the filters to write the tests
         [Theory]
-        [InlineData("Test", ProjectStates.Next, "", "", false, true)]
-        [InlineData("Test", ProjectStates.Closed, "", "", false, false)]
-        [InlineData("Test", ProjectStates.Closed, "", "", true, true)]
-        [InlineData("Test", ProjectStates.Closed, "A", "", true, false)]
-        [InlineData("Test", ProjectStates.Active, "", "", false, true)]
-        [InlineData("Test", ProjectStates.Active, "A", "", false, false)]
-        [InlineData("Test", ProjectStates.Active, "Test", "", false, true)]
-        [InlineData("Test", ProjectStates.Active, "Testa", "", false, false)]
-        [InlineData("Test", ProjectStates.Active, "T est", "", false, false)]
-        [InlineData("", ProjectStates.Active, "", "", false, true)]
-        [InlineData("", ProjectStates.Active, "Test", "", false, false)]
-        public void FilterProject_ReturnsFilteredProject(
-            string name,
-            ProjectStates state,
-            string searchTerm,
-            string tagFilter,
-            bool showClosed,
-            bool expected)
+        [InlineData(true, true, true)]
+        [InlineData(true, false, false)]
+        [InlineData(false, true, false)]
+        [InlineData(false, false, false)]
+        public void FilterProject_ReturnsFilteredProject(bool filterResult1, bool filterResult2, bool expected)
         {
-            Project project = new()
-            {
-                Name = name,
-                State = state,
-            };
+            var filterOptions = GetProjectFilterOptions(filterResult1, filterResult2);
+            var project = new Project();
+            var projectFilterArgs = new ProjectFilterArgs();
 
-            var sut = new ProjectFilter();
+            var sut = new ProjectFilter(filterOptions);
 
-            var actual = sut.FilterProject(project, searchTerm, tagFilter, showClosed);
+            var act = sut.FilterProject(project, projectFilterArgs);
 
-            actual.Should().Be(expected);
+            act.Should().Be(expected);
+        }
+
+        private static List<IProjectFilterOption> GetProjectFilterOptions(bool result1, bool result2)
+        {
+            var filter1 = new Mock<IProjectFilterOption>();
+            filter1.Setup(x => x.Filter(It.IsAny<Project>(), It.IsAny<ProjectFilterArgs>())).Returns(result1);
+
+            var filter2 = new Mock<IProjectFilterOption>();
+            filter2.Setup(x => x.Filter(It.IsAny<Project>(), It.IsAny<ProjectFilterArgs>())).Returns(result2);
+
+            var filterOptions = new List<IProjectFilterOption> { filter1.Object, filter2.Object };
+            return filterOptions;
         }
     }
 }
