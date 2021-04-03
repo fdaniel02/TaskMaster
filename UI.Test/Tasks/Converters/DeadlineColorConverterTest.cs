807900ec89d200fb4b31e1f45890c9370d471d44
DanielF <fdaniel02@gmail.com>
2021-04-03 19:17:31 +0200

    unit tests

---
+++ UI.Test/Tasks/Converters/DeadlineColorConverterTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Media;
+using FluentAssertions;
+using UI.Tasks.Constants;
+using UI.Tasks.Converters;
+using Xunit;
+
+namespace UI.Test.Tasks.Converters
+{
+    public class DeadlineColorConverterTest
+    {
+        [Fact]
+        public void Convert_InputNotValidDate_ReturnsDefaultColor()
+        {
+            var expected = DeadlineColors.Default;
+
+            var sut = new DeadlineColorConverter();
+
+            var actual = sut.Convert(null, null, null, null) as SolidColorBrush;
+
+            actual?.Color.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ConvertBack_ReturnsNull()
+        {
+            var sut = new DeadlineColorConverter();
+
+            var actual = sut.ConvertBack(null, null, null, null);
+
+            actual.Should().BeNull();
+        }
+    }
+}
