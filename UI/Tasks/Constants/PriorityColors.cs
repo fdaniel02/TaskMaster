9f7421cf0bd56e1507d3b3e333a82a5daa1e5d64
DanielF <fdaniel02@gmail.com>
2021-04-03 16:11:39 +0200

    refactor

---
+++ UI/Tasks/Constants/PriorityColors.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+
+namespace UI.Tasks.Constants
+{
+    public static class PriorityColors
+    {
+        public static readonly Color Critical = Color.FromRgb(211, 93, 110);
+
+        public static readonly Color High = Color.FromRgb(239, 176, 140);
+
+        public static readonly Color Normal = Color.FromRgb(90, 164, 105);
+
+        public static readonly Color Minor = Color.FromRgb(197, 215, 189);
+
+        public static readonly Color Default = Color.FromRgb(255, 255, 255);
+    }
+}
--- UI/Tasks/Converters/PriorityColorConverter.cs
+++ UI/Tasks/Converters/PriorityColorConverter.cs
@@ -3,17 +3,18 @@ using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
 using Domain.Enums;
+using UI.Tasks.Constants;
 
 namespace UI.Tasks.Converters
 {
-    internal class PriorityColorConverter : IValueConverter
+    public class PriorityColorConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var brush = new SolidColorBrush();
             if (value is not ProjectPriorities priority)
             {
-                brush.Color = Color.FromRgb(255, 255, 255);
+                brush.Color = PriorityColors.Default;
                 return brush;
             }
 
@@ -24,16 +25,19 @@ namespace UI.Tasks.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return null;
         }
 
-        private Color ConvertPriorityToColor(ProjectPriorities priority) =>
-            priority switch
+        private Color ConvertPriorityToColor(ProjectPriorities priority)
+        {
+            return priority switch
             {
-                ProjectPriorities.Critical => Color.FromRgb(211, 93, 110),
-                ProjectPriorities.High => Color.FromRgb(239, 176, 140),
-                ProjectPriorities.Normal => Color.FromRgb(90, 164, 105),
-                _ => Color.FromRgb(197, 215, 189),
+                ProjectPriorities.Critical => PriorityColors.Critical,
+                ProjectPriorities.High => PriorityColors.High,
+                ProjectPriorities.Normal => PriorityColors.Normal,
+                ProjectPriorities.Minor => PriorityColors.Minor,
+                _ => PriorityColors.Default,
             };
+        }
     }
 }
