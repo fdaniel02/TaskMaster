7c2965e996547512e03fcd481359422993566f43
DanielF <fdaniel02@gmail.com>
2021-04-10 15:43:06 +0200

    order column added to the Project table

---
+++ Domain/Migrations/20210408183117_AddProjectOrder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskMaster.Domain.Migrations
+{
+    public partial class AddProjectOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Project");
+        }
+    }
+}
