5f17c753e6c0671182fffb091f682c2839402b3e
DanielF <fdaniel02@gmail.com>
2021-04-10 15:46:00 +0200

    ProjectOrderService added

---
+++ Services/IProjectOrderService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Services
+{
+    public interface IProjectOrderService
+    {
+        void MoveDown(Project project, int newPosition);
+
+        void MoveUp(Project project, int newPosition);
+    }
+}
